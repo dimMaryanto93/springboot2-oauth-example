a0dd830d98f356c89933a42971026407f8de1860
dimasm93 <software.dimas_m@icloud.com>
2022-10-13 21:03:50 +0700

    security context

--- src/main/java/com/maryanto/dimas/example/security/SecurityAESUtil.java
+++ src/main/java/com/maryanto/dimas/example/security/SecurityAESUtil.java
@@ -1,5 +1,7 @@
 package com.maryanto.dimas.example.security;
 
+import lombok.extern.slf4j.Slf4j;
+
 import javax.crypto.*;
 import javax.crypto.spec.SecretKeySpec;
 import java.security.InvalidKeyException;
@@ -8,36 +10,49 @@ import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.Base64;
 
+@Slf4j
 public class SecurityAESUtil {
 
     private static final String algorithm = "AES/ECB/PKCS5Padding";
     private static final String sha1 = "SHA-1";
 
-    public static SecretKey getKeyFromPassword(String salt)
-            throws NoSuchAlgorithmException {
-        MessageDigest sha = MessageDigest.getInstance(sha1);
-        byte[] key = sha.digest(salt.getBytes());
-        key = Arrays.copyOf(key, 16);
-        return new SecretKeySpec(key, "AES");
+    public static SecretKey getKeyFromPassword(String salt) {
+        try {
+            MessageDigest sha = MessageDigest.getInstance(sha1);
+            byte[] key = sha.digest(salt.getBytes());
+            key = Arrays.copyOf(key, 16);
+            return new SecretKeySpec(key, "AES");
+        } catch (NoSuchAlgorithmException nae) {
+            log.error("can't generate/handle secret key", nae);
+        }
+        return null;
     }
 
-    public static String encrypt(String input, SecretKey key)
-            throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+    public static String encrypt(String input, SecretKey key) {
+        try {
+            Cipher cipher = Cipher.getInstance(algorithm);
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            byte[] cipherText = cipher.doFinal(input.getBytes());
+            return Base64.getEncoder()
+                    .encodeToString(cipherText);
 
-        Cipher cipher = Cipher.getInstance(algorithm);
-        cipher.init(Cipher.ENCRYPT_MODE, key);
-        byte[] cipherText = cipher.doFinal(input.getBytes());
-        return Base64.getEncoder()
-                .encodeToString(cipherText);
+        } catch (NoSuchPaddingException | NoSuchAlgorithmException | InvalidKeyException | BadPaddingException | IllegalBlockSizeException ex) {
+            log.error("can't encrypt text", ex);
+        }
+        return null;
     }
 
-    public static String decrypt(String cipherText, SecretKey key)
-            throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+    public static String decrypt(String cipherText, SecretKey key) {
+        try {
+            Cipher cipher = Cipher.getInstance(algorithm);
+            cipher.init(Cipher.DECRYPT_MODE, key);
+            byte[] plainText = cipher.doFinal(Base64.getDecoder()
+                    .decode(cipherText));
+            return new String(plainText);
 
-        Cipher cipher = Cipher.getInstance(algorithm);
-        cipher.init(Cipher.DECRYPT_MODE, key);
-        byte[] plainText = cipher.doFinal(Base64.getDecoder()
-                .decode(cipherText));
-        return new String(plainText);
+        } catch (NoSuchPaddingException | NoSuchAlgorithmException | InvalidKeyException | BadPaddingException | IllegalBlockSizeException ex) {
+            log.error("can't decrypt text", ex);
+        }
+        return null;
     }
 }
