d5b7ed992177e69aeff5ea6c3fb3157b0fdfec67
dimasm93 <software.dimas_m@icloud.com>
2022-10-13 14:45:51 +0700

    update init project

--- src/main/java/com/maryanto/dimas/example/SpringOauth2Application.java
+++ src/main/java/com/maryanto/dimas/example/SpringOauth2Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringOauth2FlowExampleApplication {
+public class SpringOauth2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringOauth2FlowExampleApplication.class, args);
+		SpringApplication.run(SpringOauth2Application.class, args);
 	}
 
 }
